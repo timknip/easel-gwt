32e6a1ac874bcbb17790ad48b6ce0f8e4ac2c09f
timk <tim@floorplanner.com>
2011-02-03 01:48:41 +0100

    Some Graphics methods (more to come)

--- src/easeljs/client/display/Graphics.java
+++ src/easeljs/client/display/Graphics.java
@@ -62,4 +62,39 @@ public class Graphics {
 		overlay.lineTo(x, y);
 		return this;
 	}
+	
+	public Graphics endStroke() {
+		overlay.endStroke();
+		return this;
+	}
+	
+	/**
+	* Sets the stroke style for the current subpath. Like all drawing methods, 
+	* this can be chained, so you can define the stroke style and color 
+	* in a single line of code like so:
+	* myGraphics.setStrokeStyle(8).beginStroke("#F00");
+	* 
+	* @param thickness The width of the stroke.
+	*/ 
+	public Graphics setStrokeStyle(float thickness) {
+		overlay.setStrokeStyle(thickness);
+		return this;
+	}
+	
+	/**
+	* Sets the stroke style for the current subpath. Like all drawing methods, 
+	* this can be chained, so you can define the stroke style and color 
+	* in a single line of code like so:
+	* myGraphics.setStrokeStyle(8,"round").beginStroke("#F00");
+	* 
+	* @param thickness The width of the stroke.
+	* @param caps Optional. Indicates the type of caps to use at the end of lines. One of butt, round, or square. Defaults to "butt".
+	* @param joints Optional. Specifies the type of joints that should be used where two lines meet. One of bevel, round, or miter. Defaults to "miter".
+	* @param miter Optional. If joints is set to "miter", then you can specify a miter limit ratio which controls at what point a mitered joint will be clipped.
+	*/
+	public Graphics setStrokeStyle(
+			float thickness, String caps, String joints, String miterLimit) {
+		overlay.setStrokeStyle(thickness, caps, joints, miterLimit);
+		return this;
+	}
 }
--- src/easeljs/client/display/impl/GraphicsImpl.java
+++ src/easeljs/client/display/impl/GraphicsImpl.java
@@ -42,4 +42,32 @@ public class GraphicsImpl extends JavaScriptObject {
 	public final native GraphicsImpl lineTo(float x, float y) /*-{
 		return this.lineTo(x, y);
 	}-*/;
+	
+	/**
+	* Sets the stroke style for the current subpath. Like all drawing methods, 
+	* this can be chained, so you can define the stroke style and color 
+	* in a single line of code like so:
+	* myGraphics.setStrokeStyle(8).beginStroke("#F00");
+	* 
+	* @param thickness The width of the stroke.
+	*/ 
+	public final native GraphicsImpl setStrokeStyle(float thickness) /*-{
+		return this.setStrokeStyle(thickness);
+	}-*/;
+	
+	/**
+	* Sets the stroke style for the current subpath. Like all drawing methods, 
+	* this can be chained, so you can define the stroke style and color 
+	* in a single line of code like so:
+	* myGraphics.setStrokeStyle(8,"round").beginStroke("#F00");
+	* 
+	* @param thickness The width of the stroke.
+	* @param caps Optional. Indicates the type of caps to use at the end of lines. One of butt, round, or square. Defaults to "butt".
+	* @param joints Optional. Specifies the type of joints that should be used where two lines meet. One of bevel, round, or miter. Defaults to "miter".
+	* @param miter Optional. If joints is set to "miter", then you can specify a miter limit ratio which controls at what point a mitered joint will be clipped.
+	*/
+	public final native GraphicsImpl setStrokeStyle(
+			float thickness, String caps, String joints, String miterLimit) /*-{
+		return this.setStrokeStyle(thickness, caps, joints, miterLimit);
+	}-*/;
 }
