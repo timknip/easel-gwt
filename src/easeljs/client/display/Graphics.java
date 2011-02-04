224f0654569602be2cfe1903ba14284d65283467
timk <tim@floorplanner.com>
2011-02-04 16:37:06 +0100

    Added some more Graphics methods

--- src/easeljs/client/display/Graphics.java
+++ src/easeljs/client/display/Graphics.java
@@ -13,6 +13,63 @@ public class Graphics {
 		this.overlay = overlay;
 	}
 	
+	/**
+	* Returns a CSS compatible color string based on the specified RGB numeric color values in the format "rgba(255,255,255,1.0)", or if alpha is null then in the format "rgb(255,255,255)". For example,
+	* Graphics.getRGB(50,100,150,0.5) will return "rgba(50,100,150,0.5)".
+	* @param r The red component for the color, between 0 and 0xFF (255).
+	* @param g The green component for the color, between 0 and 0xFF (255).
+	* @param b The blue component for the color, between 0 and 0xFF (255).
+	*/
+	public static String getRGB(int r, int g, int b) {
+		return GraphicsImpl.getRGB(r, g, b);
+	}
+	
+	/**
+	* Returns a CSS compatible color string based on the specified RGB 
+	* numeric color values in the format "rgba(255,255,255,1.0)", or if 
+	* alpha is null then in the format "rgb(255,255,255)". For example,
+	* Graphics.getRGB(50,100,150,0.5) will return "rgba(50,100,150,0.5)".
+	* 
+	* @param r The red component for the color, between 0 and 0xFF (255).
+	* @param g The green component for the color, between 0 and 0xFF (255).
+	* @param b The blue component for the color, between 0 and 0xFF (255).
+	* @param alpha Optional. The alpha component for the color where 0 is fully transparent and 1 is fully opaque.
+	*/
+	public static String getRGB(int r, int g, int b, float alpha) {
+		return GraphicsImpl.getRGB(r, g, b, alpha);
+	}
+	
+	/**
+	* Returns a CSS compatible color string based on the specified HSL 
+	* numeric color values in the format "hsla(360,100,100,1.0)", or 
+	* if alpha is null then in the format "hsl(360,100,100)". For example,
+	* Graphics.getHSL(150,100,70) will return "hsl(150,100,70)".
+	* 
+	* @param hue The hue component for the color, between 0 and 360.
+	* @param saturation The saturation component for the color, between 0 and 100.
+	* @param lightness The lightness component for the color, between 0 and 100.
+	*/
+	public static String getHSL(int hue, int saturation, 
+			int lightness) {
+		return GraphicsImpl.getHSL(hue, saturation, lightness);
+	}
+	
+	/**
+	* Returns a CSS compatible color string based on the specified HSL 
+	* numeric color values in the format "hsla(360,100,100,1.0)", or if
+	* alpha is null then in the format "hsl(360,100,100)". For example,
+	* Graphics.getHSL(150,100,70) will return "hsl(150,100,70)".
+	* 
+	* @param hue The hue component for the color, between 0 and 360.
+	* @param saturation The saturation component for the color, between 0 and 100.
+	* @param lightness The lightness component for the color, between 0 and 100.
+	* @param alpha Optional. The alpha component for the color where 0 is fully transparent and 1 is fully opaque.
+	*/
+	public static String getHSL(int hue, int saturation, 
+			int lightness, float alpha) {
+		return GraphicsImpl.getHSL(hue, saturation, lightness, alpha);
+	}
+	
 	public Graphics clear() {
 		overlay.clear();
 		return this;
@@ -48,6 +105,16 @@ public class Graphics {
 		return this;
 	}
 	
+	/**
+	* Closes the current path, effectively drawing a line from the 
+	* current drawing point to the first drawing point specified since 
+	* the fill or stroke was last set.
+	*/
+	public Graphics closePath() {
+		overlay.closePath();
+		return this;
+	}
+	
 	public Graphics endFill() {
 		overlay.endFill();
 		return this;
@@ -60,6 +127,94 @@ public class Graphics {
 	
 	public Graphics lineTo(float x, float y) {
 		overlay.lineTo(x, y);
+		return this;
+	}
+	
+	/**
+	* Draws an arc with the specified control points and radius.  
+	* For detailed information, read the 
+	* <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param x1
+	* @param y1
+	* @param x2
+	* @param y2
+	* @param radius
+	*/
+	public Graphics arcTo(float x1, float y1,
+			float x2, float y2, float radius) {
+		overlay.arcTo(x1, y1, x2, y2, radius);
+		return this;
+	}
+	
+	/**
+	* Draws an arc defined by the radius, startAngle and endAngle arguments, 
+	* centered at the position (x,y). 
+	* For example arc(100,100,20,0,Math.PI*2) would draw a full circle with 
+	* a radius of 20 centered at 100,100. For detailed information, 
+	* read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param x
+	* @param y
+	* @param radius
+	* @param startAngle
+	* @param endAngle
+	* @param anticlockwise
+	*/
+	public Graphics arc(float x, float y, float radius, 
+			float startAngle, float endAngle, boolean anticlockwise) {
+		overlay.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+		return this;
+	}
+	
+	/**
+	* Draws a quadratic curve from the current drawing point to (x,y) using 
+	* the control point (cpx,cpy).  For detailed information, 
+	* read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cpx
+	* @param cpy
+	* @param x
+	* @param y
+	*/
+	public Graphics quadraticCurveTo(float cpx, float cpy, 
+			float x, float y) {
+		overlay.quadraticCurveTo(cpx, cpy, x, y);
+		return this;
+	}
+	
+	/**
+	* Draws a bezier curve from the current drawing point to (x,y) 
+	* using the control points (cp1x,cp1y) and (cp2x,cp2y).  
+	* For detailed information, read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cp1x
+	* @param cp1y
+	* @param cp2x
+	* @param cp2y
+	* @param x
+	* @param y
+	*/
+	public Graphics bezierCurveTo(
+			float cp1x, float cp1y, 
+			float cp2x, float cp2y, 
+			float x, float y) {
+		overlay.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+		return this;
+	}
+
+	/**
+	* Draws a rectangle at (x,y) with the specified width and height 
+	* using the current fill and/or stroke.  
+	* For detailed information, read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cpx
+	* @param cpy
+	* @param x
+	* @param y
+	*/
+	public Graphics rect(float x, float y, float w, float h) {
+		overlay.rect(x, y, w, h);
 		return this;
 	}
 	
--- src/easeljs/client/display/impl/GraphicsImpl.java
+++ src/easeljs/client/display/impl/GraphicsImpl.java
@@ -5,6 +5,63 @@ import com.google.gwt.core.client.JavaScriptObject;
 public class GraphicsImpl extends JavaScriptObject {
 	protected GraphicsImpl() {}
 	
+	/**
+	* Returns a CSS compatible color string based on the specified RGB numeric color values in the format "rgba(255,255,255,1.0)", or if alpha is null then in the format "rgb(255,255,255)". For example,
+	* Graphics.getRGB(50,100,150,0.5) will return "rgba(50,100,150,0.5)".
+	* @param r The red component for the color, between 0 and 0xFF (255).
+	* @param g The green component for the color, between 0 and 0xFF (255).
+	* @param b The blue component for the color, between 0 and 0xFF (255).
+	*/
+	public static final native String getRGB(int r, int g, int b) /*-{
+		return $wnd.Graphics.getRGB(r, g, b);
+	}-*/;
+	
+	/**
+	* Returns a CSS compatible color string based on the specified RGB 
+	* numeric color values in the format "rgba(255,255,255,1.0)", or if 
+	* alpha is null then in the format "rgb(255,255,255)". For example,
+	* Graphics.getRGB(50,100,150,0.5) will return "rgba(50,100,150,0.5)".
+	* 
+	* @param r The red component for the color, between 0 and 0xFF (255).
+	* @param g The green component for the color, between 0 and 0xFF (255).
+	* @param b The blue component for the color, between 0 and 0xFF (255).
+	* @param alpha Optional. The alpha component for the color where 0 is fully transparent and 1 is fully opaque.
+	*/
+	public static final native String getRGB(int r, int g, int b, float alpha) /*-{
+		return $wnd.Graphics.getRGB(r, g, b, alpha);
+	}-*/;
+	
+	/**
+	* Returns a CSS compatible color string based on the specified HSL 
+	* numeric color values in the format "hsla(360,100,100,1.0)", or 
+	* if alpha is null then in the format "hsl(360,100,100)". For example,
+	* Graphics.getHSL(150,100,70) will return "hsl(150,100,70)".
+	* 
+	* @param hue The hue component for the color, between 0 and 360.
+	* @param saturation The saturation component for the color, between 0 and 100.
+	* @param lightness The lightness component for the color, between 0 and 100.
+	*/
+	public static final native String getHSL(int hue, int saturation, 
+			int lightness) /*-{
+		return $wnd.Graphics.getHSL(hue, saturation, lightness);
+	}-*/;
+	
+	/**
+	* Returns a CSS compatible color string based on the specified HSL 
+	* numeric color values in the format "hsla(360,100,100,1.0)", or if
+	* alpha is null then in the format "hsl(360,100,100)". For example,
+	* Graphics.getHSL(150,100,70) will return "hsl(150,100,70)".
+	* 
+	* @param hue The hue component for the color, between 0 and 360.
+	* @param saturation The saturation component for the color, between 0 and 100.
+	* @param lightness The lightness component for the color, between 0 and 100.
+	* @param alpha Optional. The alpha component for the color where 0 is fully transparent and 1 is fully opaque.
+	*/
+	public static final native String getHSL(int hue, int saturation, 
+			int lightness, float alpha) /*-{
+		return $wnd.Graphics.getHSL(hue, saturation, lightness, alpha);
+	}-*/;
+	
 	public final native GraphicsImpl clear() /*-{
 		return this.clear();
 	}-*/;
@@ -44,6 +101,89 @@ public class GraphicsImpl extends JavaScriptObject {
 	}-*/;
 	
 	/**
+	* Draws an arc with the specified control points and radius.  
+	* For detailed information, read the 
+	* <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param x1
+	* @param y1
+	* @param x2
+	* @param y2
+	* @param radius
+	*/
+	public final native GraphicsImpl arcTo(float x1, float y1,
+			float x2, float y2, float radius) /*-{
+		return this.arcTo(x1, y1, x2, y2, radius);
+	}-*/;
+	
+	/**
+	* Draws an arc defined by the radius, startAngle and endAngle arguments, 
+	* centered at the position (x,y). 
+	* For example arc(100,100,20,0,Math.PI*2) would draw a full circle with 
+	* a radius of 20 centered at 100,100. For detailed information, 
+	* read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param x
+	* @param y
+	* @param radius
+	* @param startAngle
+	* @param endAngle
+	* @param anticlockwise
+	*/
+	public final native GraphicsImpl arc(float x, float y, float radius, 
+			float startAngle, float endAngle, boolean anticlockwise) /*-{
+		return this.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+	}-*/;
+	
+	/**
+	* Draws a quadratic curve from the current drawing point to (x,y) using 
+	* the control point (cpx,cpy).  For detailed information, 
+	* read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cpx
+	* @param cpy
+	* @param x
+	* @param y
+	*/
+	public final native GraphicsImpl quadraticCurveTo(float cpx, float cpy, 
+			float x, float y) /*-{
+		return this.quadraticCurveTo(cpx, cpy, x, y);
+	}-*/;
+	
+	/**
+	* Draws a bezier curve from the current drawing point to (x,y) 
+	* using the control points (cp1x,cp1y) and (cp2x,cp2y).  
+	* For detailed information, read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cp1x
+	* @param cp1y
+	* @param cp2x
+	* @param cp2y
+	* @param x
+	* @param y
+	*/
+	public final native GraphicsImpl bezierCurveTo(
+			float cp1x, float cp1y, 
+			float cp2x, float cp2y, 
+			float x, float y) /*-{
+		return this.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+	}-*/;
+	
+	/**
+	* Draws a rectangle at (x,y) with the specified width and height 
+	* using the current fill and/or stroke.  
+	* For detailed information, read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#complex-shapes-(paths)">whatwg spec</a>.
+	* 
+	* @param cpx
+	* @param cpy
+	* @param x
+	* @param y
+	*/
+	public final native GraphicsImpl rect(float x, float y, float w, float h) /*-{
+		return this.rect(x, y, w, h);
+	}-*/;
+	
+	/**
 	* Sets the stroke style for the current subpath. Like all drawing methods, 
 	* this can be chained, so you can define the stroke style and color 
 	* in a single line of code like so:
@@ -53,6 +193,15 @@ public class GraphicsImpl extends JavaScriptObject {
 	*/ 
 	public final native GraphicsImpl setStrokeStyle(float thickness) /*-{
 		return this.setStrokeStyle(thickness);
+	}-*/;
+	
+	/**
+	* Closes the current path, effectively drawing a line from the 
+	* current drawing point to the first drawing point specified since 
+	* the fill or stroke was last set.
+	*/
+	public final native GraphicsImpl closePath() /*-{
+		return this.closePath();
 	}-*/;
 	
 	/**
